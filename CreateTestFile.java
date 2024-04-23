016e1a3a5d4fb421c2e97988a8f50266f9c7ec51
Jayesh Soni <jsoni4502@gmail.com>
2024-04-23 18:29:30 +0530

    New file added

---
+++ CreateTestFile.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * This program creates a new file 'myTestFile' and writes some information into it.
+ * 
+ * @author Jayesh Soni
+ * @since 2024-04-23
+ */
+public class CreateTestFile {
+
+	public static void main(String[] args) {
+		try {
+			// Create new file
+			FileWriter myTestFile = new FileWriter("myTestFile.txt");
+
+			// Write personal inforamtion into file
+			myTestFile.write("Name : Jayesh Soni\n");
+			myTestFile.write("Age : 20\n");
+			myTestFile.write("Address : Avadh PG, lilanagar , ahemdabad\n");
+			myTestFile.close();
+			System.out.println("Successfully created file");
+		} catch (IOException e) {
+			System.out.println("Operation failed");
+		}
+	}
+
+}
